17d51d319d3189230fc055687db0aacfce862c25
Juan Carlos Rojas <rojb.jc@gmail.com>
2021-04-12 19:32:25 -0600

    Initial Commit

---
+++ pokemonList/pokemonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonListApp.swift
+//  pokemonList
+//
+//  Created by Juan Carlos  Rojas on 12/4/21.
+//
+
+import SwiftUI
+
+@main
+struct pokemonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
